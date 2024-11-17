fa22e6bb078a91c3cc7faeb19097c3329c3baa89
Steven Watson <steven.watson@travelandleisure.com>
2024-11-17 17:35:15 -0600

    deploy fix

--- app/data/database.server.ts
+++ app/data/database.server.ts
@@ -1,29 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-/**
- * @type PrismaClient
- */
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
-  try {
-    prisma = new PrismaClient();
-    await prisma.$connect();
-    console.log('Successfully connected to database in production');
-  } catch (error) {
-    console.error('Failed to connect to database:', error);
-    throw error;
-  }
+  prisma = new PrismaClient();
+  prisma
+    .$connect()
+    .then(() => console.log('Successfully connected to database in production'))
+    .catch((error) => console.error('Failed to connect to database:', error));
 } else {
   if (!global.__db) {
-    try {
-      global.__db = new PrismaClient();
-      await global.__db.$connect();
-      console.log('Successfully connected to database in development');
-    } catch (error) {
-      console.error('Failed to connect to database:', error);
-      throw error;
-    }
+    global.__db = new PrismaClient();
+    global.__db
+      .$connect()
+      .then(() =>
+        console.log('Successfully connected to database in development')
+      )
+      .catch((error) => console.error('Failed to connect to database:', error));
   }
   prisma = global.__db;
 }
